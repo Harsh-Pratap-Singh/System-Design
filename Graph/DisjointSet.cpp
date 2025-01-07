6da41762a9857d0ea600d4cf2ec1d6163351d632
Harsh-Pratap-Singh <harshpratapsingh.main@gmail.com>
2025-01-07 21:01:47 +0530

    disjoint set

--- Graph/DisjointSet.cpp
+++ Graph/DisjointSet.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 class DisjoinSet{
     private: 
-        vector<int>rank,parent;
+        vector<int>rank,parent,size;
     
     public:
         DisjoinSet(int n){
             rank.resize(n+1,0);
             parent.resize(n+1);
+            size.resize(n+1,1);
             for(int i = 0;i <= n;i ++){
                 parent[i] = i;
             }
@@ -33,6 +34,19 @@ class DisjoinSet{
                 parent[u_up] = v_up;
                 rank[v_up] ++;
             }
+        }
+        void unionBySize(int u, int v){
+            int u_up = findUp(u);
+            int v_up = findUp(v);
+            if(u_up <  v_up){
+                parent[u_up] = v_up;
+                size[v_up] += size[u_up];
+            }
+            else{
+                parent[v_up] = u_up;
+                size[u_up] += size[v_up];
+            }
+
         }
         bool isComponent(int v , int u){
             return findUp(v) == findUp(u);
